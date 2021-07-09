297d4c41df74c07dc8d91f9b437102be5a87c712
Quddus1916 <quddusjunior1916@gmail.com>
2021-07-09 05:30:59 -0500

    basic quiz page with jason data

---
+++ Basic Quiz App/js/question-answer.js
@@ -0,0 +1,26 @@
+var jsonData=[
+    {
+        "q":"which is 1?",
+        "opt1":"1",
+        "opt2":"2",
+        "opt3":"3",
+        "opt4":"4",
+        "answer":"1"
+    },
+    {
+        "q":"who am I?",
+        "opt1":"nafiul",
+        "opt2":"rafsan",
+        "opt3":"shafi",
+        "opt4":"sadaf",
+        "answer":"nafiul"
+    },
+    {
+        "question":"who is bad?",
+        "opt1":"nafiul",
+        "opt2":"shafi",
+        "opt3":"rafsan",
+        "opt4":"sadaf",
+        "answer":"shafi"
+    },
+];
