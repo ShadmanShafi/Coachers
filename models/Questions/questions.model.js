2a4031eeb52ac337e81ac64e27d1a02b0711bebc
Rafsan <rafsan.quayes@gmail.com>
2021-07-09 14:18:55 +0600

    Doing stuff

--- models/Questions/questions.model.js
+++ models/Questions/questions.model.js
@@ -1,28 +1,27 @@
-var questionList =[
-    {
-        Question: "What is 1",
-        answers: {
-            a: 1,
-            b: 2,
-            c: 3,
-            d: 4
-        },
-        correctAnswer: 'a'
-    },
-    {
-        Question: "What is 3",
-        answers: {
-            a: 1,
-            b: 2,
-            c: 3,
-            d: 4
-        },
-        correctAnswer: 'c'
-    },
-]
-
 // Get questions from the database with the matching tag
 const generateQuestionList = (tag) => {
+    var questionList =[
+        {
+            Question: "What is 1",
+            answers: {
+                a: 1,
+                b: 2,
+                c: 3,
+                d: 4
+            },
+            correctAnswer: 'a'
+        },
+        {
+            Question: "What is 3",
+            answers: {
+                a: 1,
+                b: 2,
+                c: 3,
+                d: 4
+            },
+            correctAnswer: 'c'
+        },
+    ]
     return questionList;
 }
 
